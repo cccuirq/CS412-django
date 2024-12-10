99d58f2d324ca537109c6530836094e7f23d32fc
ccryscui <ccryscui@bu.edu>
2024-12-10 14:05:25 -0500

    waterlog

--- waterlog/context_processors.py
+++ waterlog/context_processors.py
@@ -1,10 +1,10 @@
-# waterlog/context_processors.py
-from .models import Notification, WProfile
-from django.shortcuts import get_object_or_404
+from .models import Notification
 
 def unread_notifications_count(request):
     if request.user.is_authenticated:
-        user_profile = get_object_or_404(WProfile, waterUser=request.user)
-        unread_count = Notification.objects.filter(wprofile=user_profile, read=False).count()
+        unread_count = Notification.objects.filter(
+            wprofile__waterUser=request.user,
+            read=False
+        ).count()
         return {'unread_notifications_count': unread_count}
     return {'unread_notifications_count': 0}
